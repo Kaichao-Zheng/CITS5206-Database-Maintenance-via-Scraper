1b0c98284023934af1103a85dc1591483862a09d
ErikaKK <491649804@qq.com>
2025-08-24 22:07:33 +0800

    update layout

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,7 +4,7 @@ import sqlalchemy as sa
 from app import db
 from app.models import User
 from app.main.forms import LoginForm
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user,logout_user
 
 @bp.route("/", methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
@@ -20,3 +20,19 @@ def index():
     return render_template(
         "index.html", form=form
     )
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+@bp.route("/workspace")
+def workspace():
+    return render_template("/workspace/data.html", nav="workspace",active_tab="data")
+
+@bp.route("/settings")
+def settings():
+    return render_template("settings.html", nav="settings")
+@bp.route("/logs")
+def logs():
+    return render_template("logs.html", nav="workspace", active_tab="logs")
